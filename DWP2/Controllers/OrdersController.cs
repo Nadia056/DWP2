02e182312354a5c0a7584e72700ecaa24429eaf4
Berenice de la Cruz de la Cruz <berenicedlcrz13@gmail.com>
2024-04-17 20:46:37 -0600

    orders

---
+++ DWP2/Controllers/OrdersController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DWP2.Data;
+using DWP2.Models;
+
+namespace DWP2.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.orders.Include(o => o.Customers);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orders = await _context.orders
+                .Include(o => o.Customers)
+                .FirstOrDefaultAsync(m => m.ORDER_ID == id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            return View(orders);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["CUSTOMER_ID"] = new SelectList(_context.customers, "CUSTOMER_ID", "NAME");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ORDER_ID,CUSTOMER_ID,STATUS,SALESMAN_ID,ORDER_DATE")] Orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orders);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CUSTOMER_ID"] = new SelectList(_context.customers, "CUSTOMER_ID", "NAME", orders.CUSTOMER_ID);
+            return View(orders);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orders = await _context.orders.FindAsync(id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+            ViewData["CUSTOMER_ID"] = new SelectList(_context.customers, "CUSTOMER_ID", "NAME", orders.CUSTOMER_ID);
+            return View(orders);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("ORDER_ID,CUSTOMER_ID,STATUS,SALESMAN_ID,ORDER_DATE")] Orders orders)
+        {
+            if (id != orders.ORDER_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orders);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrdersExists(orders.ORDER_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CUSTOMER_ID"] = new SelectList(_context.customers, "CUSTOMER_ID", "NAME", orders.CUSTOMER_ID);
+            return View(orders);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orders = await _context.orders
+                .Include(o => o.Customers)
+                .FirstOrDefaultAsync(m => m.ORDER_ID == id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            return View(orders);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var orders = await _context.orders.FindAsync(id);
+            if (orders != null)
+            {
+                _context.orders.Remove(orders);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrdersExists(int? id)
+        {
+            return _context.orders.Any(e => e.ORDER_ID == id);
+        }
+    }
+}
---
+++ DWP2/Models/Orders.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace DWP2.Models
+{
+    public class Orders
+    {
+        [Key]
+        [JsonPropertyName("order_id")]
+        public int? ORDER_ID { get; set; }
+
+        [ForeignKey("Customers")]
+        [JsonPropertyName("customer_id")]
+        public int CUSTOMER_ID { get; set; }
+        public Customers? Customers { get; set; }
+
+        [JsonPropertyName("status")]
+        public string? STATUS { get; set; }
+
+        [JsonPropertyName("salesman_id")]
+        public int? SALESMAN_ID { get; set; }
+
+        [JsonPropertyName("order_date")]
+        public DateTime? ORDER_DATE { get; set; }
+
+    }
+}
